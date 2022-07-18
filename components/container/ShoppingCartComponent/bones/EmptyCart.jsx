844618b73f3d512de2f451070cf3a19137d7436f
Brian Clovis <bwclovis.web@gmail.com>
2022-07-18 07:43:44 -0400

    Empty cart styles

--- components/container/ShoppingCartComponent/bones/EmptyCart.jsx
+++ components/container/ShoppingCartComponent/bones/EmptyCart.jsx
@@ -6,15 +6,15 @@ const EmptyCart = () => {
     const { toggleCart } = useContext(CartContext)
     return (
         <section className="flex justify-center h-2/4 flex-col text-lg items-center">
-            <p>There nothing in your cart</p>
+            <p className="font-semibold text-xl mb-8">There nothing in your cart</p>
             <div>
                 <Link href="/shop">
-                    <a onClick={toggleCart}>Go to shop page</a>
+                    <a onClick={toggleCart} className="ghost-link _small">Go to shop page</a>
                 </Link>
             </div>
-            <span className="p-4">or</span>
+            <span className="p-4 font-semibold">or</span>
             <Link href="/shop/category">
-                <a className="link" onClick={toggleCart}>shop by category</a>
+                <a className="underline" onClick={toggleCart}>shop by category</a>
             </Link>
         </section>
     )
