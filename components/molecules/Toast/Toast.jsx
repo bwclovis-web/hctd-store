49b6c568db9843ce167e1c3201092611e8696762
Brian Clovis <bwclovis.web@gmail.com>
2022-09-03 08:47:29 -0400

    toast style update

--- components/molecules/Toast/Toast.jsx
+++ components/molecules/Toast/Toast.jsx
@@ -6,14 +6,14 @@ import ShopContext from "provider/ShopProvider"
 const Toast = ({ item, openToast }) => {
   const { cartDisplayPrice } = useContext(ShopContext)
   const ToastClasses = classNames({
-    'fixed bg-green-700 z-20 bottom-0 right-0 opacity-0 p-4 text-white translate-x-full transition-all': true,
+    'fixed bg-green-700 z-20 bottom-0 right-0 shadow-xl opacity-0 p-4 border-2 rounded-tl-xl text-white translate-x-full transition-all': true,
     'opacity-100 translate-x-0': openToast
   })
 
   return (
     <div className={ToastClasses}>
-      <p>{item.title} has been added to cart</p>
-      <p>Price: {cartDisplayPrice}</p>
+      <p><span className="text-xl font-semibold">{item.title}</span> has been added to cart</p>
+      <p className="mt-1"><span className="text-xl font-semibold">Price:</span> {cartDisplayPrice}</p>
     </div>
   )
 }
