5229313873c5bc3783d357b684b166dd6869dbde
Brian Clovis <bwclovis.web@gmail.com>
2022-09-05 15:09:07 -0400

    cart item style

--- components/container/ShoppingCartComponent/bones/ShoppingCardItem.jsx
+++ components/container/ShoppingCartComponent/bones/ShoppingCardItem.jsx
@@ -23,10 +23,12 @@ const ShoppingCartItem = ({ data }) => {
   return(
     <li className="flex content-start w-full mb-4">
       <div className="bg-white/20 flex w-full justify-between items-center py-2 px-3 rounded">
-        <div className="flex flex-col mr-4">
+        <div className="flex flex-col">
           <span className="text-xl tracking-wider">{data.title}</span>
-          {dyeTitle && <span className="text-base">{attrs.title.value}</span>}
-          <span className="text-md mt-2">${attrs.price.value}</span>
+          <div className="flex flex-col mr-4">
+            {dyeTitle && <span className="text-base">{attrs.title.value}</span>}
+            <span className="text-md mt-2">${attrs.price.value}</span>
+          </div>
         </div>
         {dyeTitle && <QuantityControls action={updateItemQuantity} quantity={itemQuantity} />}
         <div className="flex items-center justify-between gap-5">
