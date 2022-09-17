e0426680043e8301f83a1d265a1302e54d262f26
Brian Clovis <bwclovis.web@gmail.com>
2022-09-17 16:47:18 -0400

    undo temp change

--- components/molecules/Countdown/Countdown.jsx
+++ components/molecules/Countdown/Countdown.jsx
@@ -6,7 +6,7 @@ const Countdown = ({ endDate }) => {
   const [ days, hours, min, sec ] = useCountdown(endDate)
   const endOfDays = days + hours + min + sec <= 0
 
-  return !endOfDays ? <DoneFragment /> : <Timer days={days} hours={hours} min={min} sec={sec}/>
+  return endOfDays ? <DoneFragment /> : <Timer days={days} hours={hours} min={min} sec={sec}/>
 }
 
 export default Countdown
