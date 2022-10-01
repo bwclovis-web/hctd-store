05eefebd5ff9572272d90406bb9b7a8a68641b1c
Brian Clovis <bwclovis.web@gmail.com>
2022-10-01 11:39:35 -0400

    product card update

--- components/molecules/ProductCard/ProductCard.jsx
+++ components/molecules/ProductCard/ProductCard.jsx
@@ -12,52 +12,47 @@ const ProductCard = ({ product, type, index, filter }) => {
   const renderPriceRange = prices => {
     const maxPrice = prices.maxVariantPrice.amount
     const minPrice = prices.minVariantPrice.amount
-    return minPrice === maxPrice ? <span className="text-md">{formatPrice(minPrice, "USD")}</span> : <span className="text-base">{formatPrice(minPrice, "USD")} - {formatPrice(maxPrice, "USD")}</span>
+    return minPrice === maxPrice ? <span className="text-lg mt-1">{formatPrice(minPrice, "USD")}</span> : <span className="text-lg mt-1">{formatPrice(minPrice, "USD")} - {formatPrice(maxPrice, "USD")}</span>
   }
-  const cardClasses = classNames({
-    "h-full relative overflow-hidden rounded border border-slate-600 shadow-md": true,
-    "scale-0 transition-transform hidden": !filter
-  })
+
   const spanClasses = classNames({
     'tracking-widest text-xl': true,
     'inline-block mx-auto': !node.priceRange
   })
-  const innerCardClasses = classNames({
-    "h-full w-full absolute z-10 flex justify-center transition-all": true,
-    "bg-orange-300/90 border-t-4 border-orange-700 text-orange-900 translate-y-[84%] lg:translate-y-[88%] xl:translate-y-[76%] 2xl:translate-y-[88%] group-hover:translate-y-0 group-hover:bg-red-300/90 group-focus:bg-red-300/90 group-hover:border-none group-focus:border-none group-focus:translate-y-0 group-focus:items-center group-hover:items-center ": node.availableForSale,
-    "bg-slate-600/70 translate-y-0 border-t-0 items-center ": !node.availableForSale
+  const linkClasses = classNames({
+    "px-1 mt-4 py-2 border-2 shadow capitalize text-center w-full lg:w-2/3 mx-auto": true,
+    "border-fuchsia-800 bg-fuchsia-200 text-fuchsia-800 hover:bg-fuchsia-800 hover:text-fuchsia-200 hover:border-fuchsia-300 hover:transition-all hover:shadow-md ": node.availableForSale,
+    "bg-slate-600 text-slate-100 border-2 border-black pointer-events-none": !node.availableForSale
   })
-  const cardSpanClasses = classNames({
-    "text-lg font-semibold capitalize rounded flex items-baseline": true,
-    "justify-between w-full px-3 group-hover:bg-fuchsia-500 group-hover:border-2 group-focus:border-2 group-hover:w-auto group-focus:w-auto group-hover:items-center group-focus:items-center group-hover:p-5 group-hover:flex-col group-focus:flex-col group-hover:text-slate-200 group-focus:text-white group-hover:shadow group-hover:hover:bg-fuchsia-700 group-focus:bg-fuchsia-700 group-hover:hover:transition-all group-hover:hover:shadow-md group-focus:p-3 group-focus:shadow": node.availableForSale,
-    "bg-slate-600 px-4 py-2 text-slate-100 border-2 border-black": !node.availableForSale
-  })
+
   const checkItem = (evt, isAvailable) => {
     !isAvailable && evt.preventDefault()
   }
   return (
-    <li className={cardClasses} key={index}>
+    <li key={index} className="flex flex-col-reverse justify-center">
       <Link href={`/shop/${nestPath}[slug]`} as={`/shop/${nestPath}${node.handle}`}>
-        <a className="group h-full" onClick={evt => checkItem(evt, node.availableForSale)}>
-          <div className={innerCardClasses} >
-            <span className={cardSpanClasses}>
+        <a onClick={evt => checkItem(evt, node.availableForSale)} className={linkClasses}>
+          <div>
+            <span className="flex flex-col">
               <span className={spanClasses}>{node.availableForSale ? node.title : 'out of stock'}</span>
               {(node.priceRange && node.availableForSale) && renderPriceRange(node.priceRange)}
             </span>
           </div>
-
-          <Image
-            src={imageUrl}
-            alt=""
-            layout="responsive"
-            height={600}
-            width={960}
-            objectFit="cover"
-            placeholder="blur"
-            blurDataURL={`/_next/image?url=${imageUrl}&w=16&q=1`}
-          />
         </a>
       </Link>
+      <div className="h-full relative">
+        <Image
+          src={imageUrl}
+          alt=""
+          layout="responsive"
+          height={600}
+          width={960}
+          objectFit="cover"
+          placeholder="blur"
+          blurDataURL={`/_next/image?url=${imageUrl}&w=16&q=1`}
+        />
+      </div>
+      
     </li>
   )
 }
