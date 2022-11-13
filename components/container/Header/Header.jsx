12c16c203ead5d527357006c0c51b9a3eb2a5b1e
Brian Clovis <bwclovis.web@gmail.com>
2022-11-13 10:22:30 -0500

    alert message

--- components/container/Header/Header.jsx
+++ components/container/Header/Header.jsx
@@ -17,12 +17,18 @@ const Header = () => {
     }
   }, [router])
   return(
-    <header className="container py-0 outline-none" ref={headerRef} tabIndex={-1}>
-      <>
-        <UtilNav />
-        <MainNavigation />
-      </>
-    </header>
+    <>
+      <div className="text-center bg-red-500 px-2 py-5 text-xl text-slate-50">
+        <p><span className="font-semibold">NOTICE: 11/13/2022</span> due to illness, there will be an approximate 5 day delay in order fulfillment. We apologize for any inconveniences.</p>
+      </div>
+      <header className="container py-0 outline-none" ref={headerRef} tabIndex={-1}>
+        
+        <>
+          <UtilNav />
+          <MainNavigation />
+        </>
+      </header>
+    </>
   ) }
 
 export default Header
