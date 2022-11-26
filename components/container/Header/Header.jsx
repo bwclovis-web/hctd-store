9b4d8946b59b5ad140627316db019f4ff17e1313
Brian Clovis <bwclovis.web@gmail.com>
2022-11-26 07:36:30 -0500

    banner remove

--- components/container/Header/Header.jsx
+++ components/container/Header/Header.jsx
@@ -18,9 +18,9 @@ const Header = () => {
   }, [router])
   return(
     <>
-      <div className="text-center bg-red-500 px-2 py-5 text-xl text-slate-50">
+      {/* <div className="text-center bg-red-500 px-2 py-5 text-xl text-slate-50">
         <p><span className="font-semibold">NOTICE: 11/13/2022</span> due to illness, there will be an approximate 5 day delay in order fulfillment. We apologize for any inconveniences.</p>
-      </div>
+      </div> */}
       <header className="container py-0 outline-none" ref={headerRef} tabIndex={-1}>
         
         <>
