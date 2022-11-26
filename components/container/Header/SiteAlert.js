d30db2a07e246461ea5dc552103837d4140fd61e
Brian Clovis <bwclovis.web@gmail.com>
2022-11-26 10:58:46 -0500

    fix

---
+++ components/container/Header/SiteAlert.js
@@ -0,0 +1,21 @@
+import classNames from "classnames"
+
+const SiteAlert = ({ bannerData }) => {
+  const { content, color } = bannerData
+
+  const BannerClasses = classNames({
+    'text-center px-2 py-5': true,
+    'text-slate-50 bg-red-500': color[0] === 'red',
+    'text-blue-200 bg-blue-700': color[0] === 'blue',
+    'text-green-200 bg-green-700': color[0] === 'green',
+    'text-yellow-800 bg-yellow-300': color[0] === 'yellow'
+  })
+
+  return (
+    <div className={BannerClasses}>
+      <p className="text-xl tracking-wide">{content}</p>
+    </div>
+  )
+}
+
+export default SiteAlert
