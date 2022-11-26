4eff4a8272dd58ffbc5285542da143e5d22e0198
Brian Clovis <bwclovis.web@gmail.com>
2022-11-26 10:55:43 -0500

    setup sanity

--- components/container/Header/Header.jsx
+++ components/container/Header/Header.jsx
@@ -1,10 +1,24 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
-
+import { getClient } from "lib/sanity"
 import MainNavigation from 'components/molecules/Navigation/MainNavigation'
 import UtilNav from 'components/molecules/UtilityNav/UtilityNav'
+import { groq } from 'next-sanity'
+import SiteAlert from './SiteAlert'
 
+const query = groq`
+  *[_type == "sitebanner"] {
+    content,
+    startDate,
+    endDate,
+    bannerDisplay,
+    bannerColor
+  }
+`
 const Header = () => {
+  const [ displayBanner, setDisplayBanner ] = useState({
+    show: false
+  })
   const headerRef = useRef()
   const router = useRouter()
   const resetFocus = () => {
@@ -16,13 +30,31 @@ const Header = () => {
       router.events.off('routeChangeComplete', resetFocus)
     }
   }, [router])
+
+  useEffect(() => {
+    testAThing()
+  }, [])
+
+
+  const testAThing = async() => {
+    const banner = await getClient().fetch(query)
+    const today = new Date().toLocaleDateString()
+    const startDate = new Date(banner[0].startDate).toLocaleDateString()
+    const endDate = new Date(banner[0].endDate).toLocaleDateString()
+
+    if(startDate < today && endDate > today && banner[0].bannerDisplay) {
+      setDisplayBanner({
+        show: true,
+        content: banner[0].content,
+        color: banner[0].bannerColor
+      })
+    }
+  }
+
   return(
     <>
-      {/* <div className="text-center bg-red-500 px-2 py-5 text-xl text-slate-50">
-        <p><span className="font-semibold">NOTICE: 11/13/2022</span> due to illness, there will be an approximate 5 day delay in order fulfillment. We apologize for any inconveniences.</p>
-      </div> */}
+      {displayBanner.show ? <SiteAlert bannerData={displayBanner}/> : null}
       <header className="container py-0 outline-none" ref={headerRef} tabIndex={-1}>
-        
         <>
           <UtilNav />
           <MainNavigation />
