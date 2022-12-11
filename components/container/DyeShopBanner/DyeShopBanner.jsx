9763735e77490ed5006515e40ec1e24415aa5bd7
Brian Clovis <bwclovis.web@gmail.com>
2022-12-11 08:17:16 -0500

    upgrade start

--- components/container/DyeShopBanner/DyeShopBanner.jsx
+++ components/container/DyeShopBanner/DyeShopBanner.jsx
@@ -15,8 +15,8 @@ const DyeShopBanner = () => (
         <div className="flex flex-col items-center justify-start max-w-1/3 text-slate-100 bg-black/70 px-8 py-12 rounded-lg shadow-lg backdrop-blur-sm text-center">
           <h2 className="text-h2-dynamic font-display leading-none pb-2">Custom Dye Shop</h2>
           <p className="text-xl font-semibold tracking-wide mb-8 text-center">Custom blended colors for all your dyeing needs.</p>
-          <Link href="/shop/category/dyes">
-            <a className="ghost-link">Go to dye shop</a>
+          <Link href="/shop/category/dyes" className="ghost-link">
+            Go to dye shop
           </Link>
         </div>
       </div>
