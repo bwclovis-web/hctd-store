62b9f4ec5557bcfbee57de440890cff204544fc8
Brian Clovis <bwclovis.web@gmail.com>
2022-12-11 08:31:08 -0500

    fix all links

--- components/container/ShoppingCartComponent/bones/EmptyCart.jsx
+++ components/container/ShoppingCartComponent/bones/EmptyCart.jsx
@@ -9,12 +9,12 @@ const EmptyCart = () => {
     <section className="flex justify-center h-2/4 flex-col text-lg items-center">
       <p className="font-semibold text-2xl mb-10">Your cart is empty</p>
       <div>
-        <Link href="/shop">
-          <a onClick={toggleCart} className="ghost-link _light">Go to shop page</a>
+        <Link href="/shop" onClick={toggleCart} className="ghost-link _light">
+          Go to shop page
         </Link>
       </div>
     </section>
-  )
+  );
 }
 
 export default EmptyCart 
