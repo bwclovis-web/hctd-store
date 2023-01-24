2c69acb313d3644ce2a82fb251337715d009ed5f
Brian Clovis <bwclovis.web@gmail.com>
2023-01-24 18:18:45 -0500

    remove console log

--- pages/store-closed.jsx
+++ pages/store-closed.jsx
@@ -3,23 +3,19 @@ import BlockContent from "@sanity/block-content-to-react"
 import HeroComponent from 'components/molecules/Hero/Hero'
 import sanityClient from 'lib/sanityClient'
 
-const StorClosedPage = ({ content }) => {
-  console.log(`%c content`, 'background: #0047ab; color: #fff; padding: 2px:', content)
-
-  return (
-    <>
-      <HeroComponent {...content.pageHero} />
-      <section className="pt-20 py-dynamic-container-y content-container text-center">
-        <h2 className="font-display text-h1-dynamic capitalize">{content.heading}</h2>
-        {/*  REFACTOR! */}
-        <div className="content">
-          <BlockContent blocks={content.body}/>
-        </div>
+const StorClosedPage = ({ content }) => (
+  <>
+    <HeroComponent {...content.pageHero} />
+    <section className="pt-20 py-dynamic-container-y content-container text-center">
+      <h2 className="font-display text-h1-dynamic capitalize">{content.heading}</h2>
+      {/*  REFACTOR! */}
+      <div className="content">
+        <BlockContent blocks={content.body}/>
+      </div>
         
-      </section>
-    </>
-  )
-}
+    </section>
+  </>
+)
 
 export async function getStaticProps() {
   const contentProps = await sanityClient.fetch(`{
