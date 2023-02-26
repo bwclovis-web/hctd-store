f49000cd768cf4c7030f2b225d807dbbeea1b4fd
Brian Clovis <bwclovis.web@gmail.com>
2023-02-26 10:21:45 -0500

    padding updates

--- components/container/Modal/Modal.jsx
+++ components/container/Modal/Modal.jsx
@@ -36,8 +36,8 @@ const Modal = ({ children }) => {
   
   const ModalContentClasses = classNames({
     'fixed  z-30 rounded transition-all delay-300 p-4 xl:p-8 duration-500 w-full lg:w-4/5 xl:w-2/5 pointer-none flex': true,
-    'opacity-0 lg:-translate-y-80  h-auto': !animate,
-    'opacity-100 lg:-translate-y-0 transition-animate delay-200 bg-white/80 shadow-2xl duration-300 top-3 overflowY-auto h-3/4': animate
+    'opacity-0 lg:-translate-y-80   h-3/4': !animate,
+    'opacity-100 lg:-translate-y-0 transition-animate delay-200 bg-white/80 shadow-2xl duration-300 top-3 overflowY-auto h-auto': animate
   })
 
 
