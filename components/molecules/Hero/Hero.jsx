73c108aa86dfe5bdc75f2f7b2cf40c3b9755014a
Brian Clovis <bwclovis.web@gmail.com>
2023-06-23 14:32:23 -0400

    optimize - image priority

--- components/molecules/Hero/Hero.jsx
+++ components/molecules/Hero/Hero.jsx
@@ -27,6 +27,7 @@ const HeroComponent = ({ heroImage, eyebrow, heading, config, pos, link }) => {
         layout="fill"
         objectFit="cover"
         placeholder="blur"
+        priority
         blurDataURL={heroImage.asset.metadata.lqip}
       />
       <div className="relative bg-violet-800/70 text-slate-100 backdrop-blur-sm p-6 lg:p-8 xl:p-20 w-full lg:w-1/2 xl:w-5/12 flex justify-center flex-col items-start">
